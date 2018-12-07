e370160308dd7d3cc459335baa0737f7365a2ebf
Daniel Torres <dasetova@gmail.com>
2018-12-07 14:17:58 -0500

    @Value tag to refactor title to application.properties

--- src/main/java/com/dasetova/springstudy/controllers/IndexController.java
+++ src/main/java/com/dasetova/springstudy/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.dasetova.springstudy.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 	
+	@Value("${application.controller.title}")
+	private String title;
+	
 	//Model permite pasar atributos a la vista
 	//Retorna la vista
 	//GetMapping establece que permite un metodo GET
 	//Tambien se puede usar RequestMapping especificando el metodo GET
 	@GetMapping("/index")
 	public String index(Model model) {
-		model.addAttribute("title", "Hello World!, SpringBoot");
+		model.addAttribute("title", this.title);
 		return "index";
 	}
 }
